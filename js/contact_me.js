c2d71d81b7ff9bbd481440461fca698b35ba9d28
devil1993 <samanwayghatak@gmail.com>
2017-03-17 00:23:50 +0530

    Fixing data send script

--- js/contact_me.js
+++ js/contact_me.js
@@ -39,7 +39,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that system is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + name + ", it seems that system is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
